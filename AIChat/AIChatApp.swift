4b2d4385a792cda9f14325c1267e7c78652e66b0
Artem Panasenko <artem253510@gmail.com>
2025-01-04 15:45:53 +0100

    initial commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Artem Panasenko on 04.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
