67e23a0c48e50a7713500ccf5b959b3c8ffef137
Artem Panasenko <artem253510@gmail.com>
2025-01-05 14:43:39 +0100

    feat: impl Tabbar and navigation stack

---
+++ AIChat/Core/Explore/ExploreView.swift
@@ -0,0 +1,22 @@
+//
+//  ExploreView.swift
+//  AIChat
+//
+//  Created by Artem Panasenko on 05.01.2025.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    
+    var body: some View {
+        NavigationStack {
+            Text("Explore")
+                .navigationTitle("Explore")
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ AIChat/Core/Welcome/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  AIChat
+//
+//  Created by Artem Panasenko on 05.01.2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Welcome to AIChat!")
+        }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
