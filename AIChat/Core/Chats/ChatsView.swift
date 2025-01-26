003ef6c40bf8e35ca5b7fbb9872461ffb46097dc
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2025-01-26 16:03:43 +0100

    feat: RecentsAvatars

--- AIChat/Core/Chats/ChatsView.swift
+++ AIChat/Core/Chats/ChatsView.swift
@@ -9,11 +9,37 @@ import SwiftUI
 
 struct ChatsView: View {
     @State private var chats: [Chat] = Chat.mocks
+    @State private var recentAvatars: [Avatar] = Avatar.mocks
+    
     @State private var path: [NavigationPathOption] = []
+    
+    private let recentsSectionHeight: CGFloat = 120
     
     var body: some View {
         NavigationStack(path: $path) {
             List {
+                if !recentAvatars.isEmpty {
+                    recentsSection
+                }
+                
+                chatsSection
+            }
+            .navigationTitle("Chats")
+            .navigationDestinationForCoreModule(path: $path)
+        }
+    }
+    
+    private var chatsSection: some View {
+        Section {
+            if chats.isEmpty {
+                Text("No chats yet")
+                    .foregroundStyle(.secondary)
+                    .font(.title3)
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 40)
+                    .removeListRowFormatting()
+            } else {
                 ForEach(chats) { chat in
                     ChatRowCellViewBuilder(
                         currentUserId: nil, // FIXME: Add cuid
@@ -33,14 +59,51 @@ struct ChatsView: View {
                     .removeListRowFormatting()
                 }
             }
-            .navigationTitle("Chats")
-            .navigationDestinationForCoreModule(path: $path)
+        } header: {
+            Text(!chats.isEmpty ? "chats" : "")
         }
+    }
+    
+    private var recentsSection: some View {
+        Section {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 8) {
+                    ForEach(recentAvatars, id: \.self) { avatar in
+                        if let profileImageUrl = avatar.profileImageUrl {
+                            VStack(spacing: 8) {
+                                ImageLoaderView(urlString: profileImageUrl)
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .clipShape(Circle())
+                                
+                                Text(avatar.name ?? "")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            .anyButton {
+                                onAvatarPressed(avatar: avatar)
+                            }
+                        }
+                    }
+                }
+                .padding(.top, 12)
+            }
+            .frame(height: recentsSectionHeight)
+            .scrollIndicators(.hidden)
+            .removeListRowFormatting()
+        } header: {
+            Text("recents")
+        }
+
     }
     
     // MARK: - Busyness logik
     private func onChatPressed(chat: Chat) {
         let pathOption: NavigationPathOption = .chat(avatarId: chat.avatarId)
+        path.append(pathOption)
+    }
+    
+    private func onAvatarPressed(avatar: Avatar) {
+        let pathOption: NavigationPathOption = .chat(avatarId: avatar.id)
         path.append(pathOption)
     }
 }
