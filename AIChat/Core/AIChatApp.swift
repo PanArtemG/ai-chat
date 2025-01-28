098587ffecca641f43b6bb926e2bd64e9f81dee8
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2025-01-28 16:41:24 +0100

    feat: add FireBase

--- AIChat/Core/AIChatApp.swift
+++ AIChat/Core/AIChatApp.swift
@@ -5,13 +5,25 @@
 //  Created by Artem Panasenko on 04.01.2025.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct AIChatApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             AppView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
 }
