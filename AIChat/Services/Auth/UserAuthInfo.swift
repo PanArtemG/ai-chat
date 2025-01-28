61e1f03664f4bc93647c7a8ecaf48273456ff3c6
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2025-01-28 17:36:15 +0100

    feat: impl sign anonymously

---
+++ AIChat/Services/Auth/UserAuthInfo.swift
@@ -0,0 +1,61 @@
+//
+//  UserAuthInfo.swift
+//  AIChat
+//
+//  Created by Artem Panasenko on 28.01.2025.
+//
+
+import FirebaseAuth
+
+/// A structure representing user authentication information.
+struct UserAuthInfo: Sendable {
+    
+    /// The unique identifier for the authenticated user.
+    let uid: String
+    
+    /// The email address of the user, if available.
+    let email: String?
+    
+    /// A boolean value indicating whether the user is signed in anonymously.
+    let isAnonymous: Bool
+    
+    /// The date and time when the user's account was created, if available.
+    let creationDate: Date?
+    
+    /// The date and time when the user last signed in, if available.
+    let lastSignInDate: Date?
+    
+    /// Initializes a new instance of `UserAuthInfo`.
+    ///
+    /// - Parameters:
+    ///   - uid: The unique identifier for the authenticated user. (Required)
+    ///   - email: The email address of the user, if available. (Optional, defaults to `nil`)
+    ///   - isAnonymous: A boolean indicating whether the user is signed in anonymously. (Optional, defaults to `false`)
+    ///   - creationDate: The date and time when the user's account was created, if available. (Optional, defaults to `nil`)
+    ///   - lastSignInDate: The date and time when the user last signed in, if available. (Optional, defaults to `nil`)
+    internal init(
+        uid: String,
+        email: String? = nil,
+        isAnonymous: Bool = false,
+        creationDate: Date? = nil,
+        lastSignInDate: Date? = nil
+    ) {
+        self.uid = uid
+        self.email = email
+        self.isAnonymous = isAnonymous
+        self.creationDate = creationDate
+        self.lastSignInDate = lastSignInDate
+    }
+    
+    /// Initializes a new instance of `UserAuthInfo` from a Firebase `User` object.
+    ///
+    /// - Parameter user: A Firebase `User` instance.
+    /// - Extracts relevant user authentication data from the provided `User` object.
+    internal init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+        self.isAnonymous = user.isAnonymous
+        self.creationDate = user.metadata.creationDate
+        self.lastSignInDate = user.metadata.lastSignInDate
+    }
+}
